f5984682fa1c226e2ce003bea5059206aae32312
ramorimo <ramorimo90@hotmail.com>
2020-06-20 09:53:21 +0200

    moved random heavy in a separeted file

---
+++ js/algorithms/random_heavy.js
@@ -0,0 +1,60 @@
+function random_heavy(tree_node, threshold) {
+    if (tree_node === undefined) {
+        return;
+    }
+    tree_node.rpoint = [0, 0]
+
+    if (is_leaf(tree_node)) {
+        tree_node.hlength = 0
+        tree_node.vlength = 0
+    } else {
+
+        //disegno il primo sottoalbero
+        random_heavy(tree_node.sx, threshold)
+
+        //disegno il secondo sottoalbero
+        random_heavy(tree_node.dx, threshold)
+
+        random_number = Math.random()
+
+        if (one_child(tree_node)) {
+            child = tree_node.sx === undefined ? tree_node.dx : tree_node.sx
+            if (random_number > threshold) {
+                child.rpoint = [1, 0]
+                tree_node.hlength = child.hlength + 1
+                tree_node.vlength = child.vlength
+            } else {
+                child.rpoint = [0, 1]
+                tree_node.hlength = child.hlength
+                tree_node.vlength = child.vlength + 1
+            }
+
+        } else {
+            if (random_number > threshold) {
+                tree_node.sx.rpoint = [0, 1]
+                tree_node.dx.rpoint = [tree_node.sx.hlength + 1, 0]
+
+                tree_node.hlength = tree_node.dx.hlength + tree_node.sx.hlength + 1
+
+                if (tree_node.dx.vlength >= tree_node.sx.vlength + 1) {
+                    tree_node.vlength = tree_node.dx.vlength
+                } else {
+                    tree_node.vlength = tree_node.sx.vlength + 1
+                }
+            } else {
+                tree_node.dx.rpoint = [0, tree_node.sx.vlength + 1]
+                tree_node.sx.rpoint = [1, 0]
+
+                if (tree_node.dx.hlength >= tree_node.sx.hlength + 1) {
+                    tree_node.hlength = tree_node.dx.hlength
+                } else {
+                    tree_node.hlength = tree_node.sx.hlength + 1
+                }
+                tree_node.vlength = tree_node.dx.vlength + tree_node.sx.vlength + 1
+            }
+
+        }
+
+    }
+
+}
--- js/tree.js
+++ js/tree.js
@@ -20,67 +20,6 @@ function size_subtrees(tree_node) {
     }
 }
 
-function random_heavy(tree_node, threshold) {
-    if (tree_node === undefined) {
-        return;
-    }
-    tree_node.rpoint = [0, 0]
-
-    if (is_leaf(tree_node)) {
-        tree_node.hlength = 0
-        tree_node.vlength = 0
-    } else {
-
-        //disegno il primo sottoalbero
-        random_heavy(tree_node.sx, threshold)
-
-        //disegno il secondo sottoalbero
-        random_heavy(tree_node.dx, threshold)
-
-        random_number = Math.random()
-
-        if (one_child(tree_node)) {
-            child = tree_node.sx === undefined ? tree_node.dx : tree_node.sx
-            if (random_number > threshold) {
-                child.rpoint = [1, 0]
-                tree_node.hlength = child.hlength + 1
-                tree_node.vlength = child.vlength
-            } else {
-                child.rpoint = [0, 1]
-                tree_node.hlength = child.hlength
-                tree_node.vlength = child.vlength + 1
-            }
-
-        } else {
-            if (random_number > threshold) {
-                tree_node.sx.rpoint = [0, 1]
-                tree_node.dx.rpoint = [tree_node.sx.hlength + 1, 0]
-
-                tree_node.hlength = tree_node.dx.hlength + tree_node.sx.hlength + 1
-
-                if (tree_node.dx.vlength >= tree_node.sx.vlength + 1) {
-                    tree_node.vlength = tree_node.dx.vlength
-                } else {
-                    tree_node.vlength = tree_node.sx.vlength + 1
-                }
-            } else {
-                tree_node.dx.rpoint = [0, tree_node.sx.vlength + 1]
-                tree_node.sx.rpoint = [1, 0]
-
-                if (tree_node.dx.hlength >= tree_node.sx.hlength + 1) {
-                    tree_node.hlength = tree_node.dx.hlength
-                } else {
-                    tree_node.hlength = tree_node.sx.hlength + 1
-                }
-                tree_node.vlength = tree_node.dx.vlength + tree_node.sx.vlength + 1
-            }
-
-        }
-
-    }
-
-}
-
 function absolute_points(tree_node, start_point) {
 
     if (tree_node === undefined) {
@@ -182,7 +121,7 @@ function main() {
             })
             d3.select("body").append("svg").attr("width", width).attr("height", height)
             size_subtrees(tree)
-            random_heavy(tree, 0.64)
+            right_heavy(tree)
             absolute_points(tree, start_point)
             draw_tree(map_nodes.values())
         })
